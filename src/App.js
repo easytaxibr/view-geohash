5b91aff65513ff5e4603f715d10f8af56d961561
jozef24 <jozef.valverde@easytaxi.com.pe>
2018-11-21 18:59:07 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import MapContainer from './components/MapContainer'
+import Form from './components/Form'
+import { stringToArray } from './Helper';
 import './App.css';
 
 class App extends Component {
+
+  state = {
+    geohashesArray: [],
+    isFormatArray: ''
+  }
+
+  getGeohashArray = (geohashesString) => {
+    try {
+      let geohashesArray = stringToArray(geohashesString);
+      this.setState({
+        geohashesArray: geohashesArray,
+        isFormatArray: true
+      })
+    } catch (error) {
+      this.setState({
+        isFormatArray: false
+      });
+      console.log('No tiene el formato definido');
+    }
+  }
+
+  showError = () => {
+    if (this.state.isFormatArray) {
+      return (
+        <div className="alert alert-danger" role="alert">
+          <p>The format is not correct</p>
+        </div>
+      )
+    }
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className="row center_div">
+        <div className="col-3 ">
+          {this.showError()}
+          <Form getGeohashArray={this.getGeohashArray} />
+        </div>
+        <div className="col-9 static">
+          <MapContainer geohashesArray={this.state.geohashesArray} />
+        </div>
       </div>
     );
   }
